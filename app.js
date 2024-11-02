78cfa7dc977fd17ca5730d3929cd73822cdd125b
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-02 20:44:56 +0500

    making the Campground Model for the express application

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
-const app = express();
+//requiring our own Campground model from the files
+const Campground = require("./models/campground");
 
+
+//connection to the database
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+})
+
+
+const app = express();
+//use the app.set to set the view engine to ejs &&&& set the directory to views using the path modules
 //setting the view engine
 app.set("view engine", "ejs");
 //set the view directory.....>require path module for that
@@ -11,6 +27,12 @@ app.set('views',path.join(__dirname, 'views'));
 
 app.get("/", (req,res) => {
     res.render("home");
+})
+
+app.get("/makecampground", async (req,res) => {
+    const camp = new Campground({title: "My Backyard", description: "Cheap camping and heavy security is provided"});
+    await camp.save();
+    res.send(camp);
 })
 
 
