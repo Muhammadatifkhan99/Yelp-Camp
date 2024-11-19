665e17f9777f070e17bac17d1f28ecc34ce3a4a0
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-19 18:02:44 +0500

    footer partials and navbar partials added

--- seeds/index.js
+++ seeds/index.js
@@ -19,15 +19,21 @@ db.once("open", () => {
 //picking a random element from any array....
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     // const c = new Campground({title: "Purple Field"});
     // await c.save();
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://images.unsplash.com/photo-1708716340992-3aa575d89cfc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, blanditiis at dolore eligendi accusamus autem quam perspiciatis aut minus, error consequatur cum quidem quas fugiat nostrum recusandae fuga? Eum, dolores.",
+            price
         })
         await camp.save();
     }
