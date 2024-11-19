29c5a12c08f0a746e437b34686f0e4f049d2c345
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-11-19 22:10:10 +0500

    campgrounds index page update

--- seeds/index.js
+++ seeds/index.js
@@ -20,7 +20,6 @@ db.once("open", () => {
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
-
 const seedDB = async () => {
     await Campground.deleteMany({});
     // const c = new Campground({title: "Purple Field"});
@@ -31,7 +30,7 @@ const seedDB = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: "https://images.unsplash.com/photo-1708716340992-3aa575d89cfc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "https://images.unsplash.com/photo-1731569348001-e49c36947289?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, blanditiis at dolore eligendi accusamus autem quam perspiciatis aut minus, error consequatur cum quidem quas fugiat nostrum recusandae fuga? Eum, dolores.",
             price
         })
