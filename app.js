cf98f32ccf831ac40dde0725875491addd6a3e77
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-02 22:46:13 +0500

    server side validations added to the app

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const ejsMate = require("ejs-mate");
 const CatchAsync = require("./utils/CatchAsync");
 //requiring the expresserror class
 const ExpressError = require("./utils/ExpressError");
+//joi tool used for schema validation on the server side
+const Joi = require("joi");
 
 
 //connection to the database
@@ -72,7 +74,22 @@ app.get("/campgrounds/:id/edit",CatchAsync( async (req,res) => {
 
 //post requests
 app.post("/campgrounds",CatchAsync (async (req,res,next) => {
-    if(!req.body.campground) throw new ExpressError("Invalid Campground Data",400);
+    // if(!req.body.campground) throw new ExpressError("Invalid Campground Data",400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(msg,400);
+    }
+    console.log(msg);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`);
