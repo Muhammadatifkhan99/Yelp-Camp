fa924883d51fceebb60dc49ab28c992b32108ccc
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-12-18 23:16:09 +0500

    adding the review model

--- models/campground.js
+++ models/campground.js
@@ -7,7 +7,11 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review"
+    }]   
 })
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
