46974b72d41b33cabee51239c028bf2aeb4e92d1
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-16 21:00:20 +0500

    setting login routes

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utils/CatchAsync");
+const passport  = require("passport");
 
 router.get("/register",(req,res) => {
     res.render("users/register");
@@ -21,6 +22,15 @@ try{
 }
     // res.send(req.body);
 }))
+
+router.get("/login",(req,res) => {
+    res.render("users/login");
+})
+
+router.post("/login",passport.authenticate("local",{failureFlash: true, failureRedirect:"/login"}), (req,res) => {
+    req.flash("success", "Welcome back");
+    res.redirect("/campgrounds");
+})
 
 
 module.exports = router;
