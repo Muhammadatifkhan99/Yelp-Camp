30e2ce2d743d6ca0fe81124342004cd029f6c470
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-17 21:46:53 +0500

    currentUser helper method

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req,res,next) => {
+    console.log("REQ.USER...",req.user);
     if(!req.isAuthenticated()){
         req.flash("error","you must sign in");
         return res.redirect("/login");
