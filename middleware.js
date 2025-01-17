333cf3462f9a800f7ccb438d6a526b72257e494d
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-01-17 22:24:29 +0500

    returnTo functionality for login

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 module.exports.isLoggedIn = (req,res,next) => {
-    console.log("REQ.USER...",req.user);
+    // console.log("REQ.USER...",req.user);
     if(!req.isAuthenticated()){
+        req.session.returnTo = req.originalUrl;
+        // console.log(req.session.returnTo = req.originalUrl);
         req.flash("error","you must sign in");
         return res.redirect("/login");
     }
